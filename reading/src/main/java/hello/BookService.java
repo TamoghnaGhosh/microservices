3fa37889e00ee58699285dfe09e5cb13028b0c66
ravimuppidi <muppidi.ravi@gmail.com>
2016-07-25 09:00:55 +0530

    added circuit breaker request failures limit to open circuit

--- reading/src/main/java/hello/BookService.java
+++ reading/src/main/java/hello/BookService.java
@@ -1,14 +1,20 @@
 package hello;
 
 import java.net.URI;
-import org.springframework.web.client.RestTemplate;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Service
 public class BookService {
 
-  @HystrixCommand(fallbackMethod = "reliable")
+  @HystrixCommand(fallbackMethod ="reliable",
+		  commandProperties = {
+          	@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "2")
+      })
   public String readingList() {
     RestTemplate restTemplate = new RestTemplate();
     URI uri = URI.create("http://localhost:8082/bookstore/recommended");
